98730b9f912d84f119a4ee40516d85a596094083
Sreynet-long <sreynetlong168@gmail.com>
2025-10-01 19:32:28 +0700

    Configure for Railway deployment with MySQL

---
+++ src/test/java/com/net/starter_springboot/StarterSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.net.starter_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
